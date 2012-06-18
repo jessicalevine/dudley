c6c931341ec0cb4cab44e8c12d79a5c2bda223ec
Sam Rose <samwho@lbak.co.uk>
2012-06-18 01:25:04 +0100

    Namespace the roll command appropriately.

--- lib/dudley/commands/roll.rb
+++ lib/dudley/commands/roll.rb
@@ -1,13 +1,15 @@
 module Dudley
-  class Roll < Command
-    command :roll do |input|
-      DiceParser.parse input
-    end
+  module Commands
+    class Roll < Command
+      command :roll do |input|
+        DiceParser.parse input
+      end
 
-    namespace :roll do
-      command :init do |input|
-        # Stub for rolling init.
-        "Implement me!"
+      namespace :roll do
+        command :init do |input|
+          # Stub for rolling init.
+          "Implement me!"
+        end
       end
     end
   end
