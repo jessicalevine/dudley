7b5dc8491bc06edb502c7196648f6340bacf8f5d
Sam Rose <samwho@lbak.co.uk>
2012-06-18 01:51:49 +0100

    Fixed broken spec.

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 describe Dudley::Command do
   it "should correctly execute a roll given a roll command" do
     result = Dudley::Command.execute("!roll 1d6")
-    result.should be_within 1..6 
+    result.should be_within 1..6
   end
 
-  it "should return nil given a nonexistant command" do
-    result = Dudley::Command.execute("!foo")
-    result.should be_nil
+  it "should throw an exception given a nonexistant command" do
+    lambda do
+      Dudley::Command.execute("!foo")
+    end.should raise_error Dudley::CommandDoesNotExist
   end
 end
